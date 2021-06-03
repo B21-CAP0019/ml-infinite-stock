34aa80d74b6eb8e5c97d603c14011e34b46cc9cc
Rakha-Rozaqtama <RakhaRozaqtama.porto@gmail.com>
2021-06-03 12:55:15 +0700

    Fixing API for forecasting

--- API/App.py
+++ API/App.py
@@ -28,50 +28,31 @@ app.config['SECRET_KEY'] = 'bangkitCapstone'
 mysql = MySQL(app)
 
 
-# Check data dari client
-auth = {
-    "type": "object",
-    "properties": {
-        "email": {
-            "type": "string",
-            "minLength": 10
-        },
-        "password": {
-            "type": "string",
-            "minLength": 8,
-            "maxLength": 15
-        }
-    },
-    "required": ["email", "password"]
-}
-
-
 @app.route('/', methods=['GET'])
 def success():
     return make_response(jsonify({"message": "Success"}), 200)
 
 
 @app.route('/auth/signup', methods=['POST'])
-#@expects_json(auth)
 def sign_up():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify username or password', 401)
     if not (len(auth.password) > 8 and len(auth.password) < 15):
-        return make_response(jsonify({'status':0, 'message':'password should contain characters in range 8 - 15'}), 400)
+        return make_response(jsonify({'status': 0, 'message': 'password should contain characters in range 8 - 15'}), 400)
     password = generate_password_hash(auth.password, method='sha256')
     public_id = str(uuid.uuid4())
     try:
         cursor = mysql.connection.cursor()
-        query = 'INSERT INTO user(public_id,email,password) VALUES (%s, %s, %s);'
+        query = 'INSERT INTO User(public_id,email,password) VALUES (%s, %s, %s);'
         params = (public_id, auth.username, password)
         cursor.execute(query, params)
         mysql.connection.commit()
     except Exception as err:
-        return make_response(jsonify({'status':0, "message": "Could not create a new user, Querying error!", "errorDB":err}), 500)
+        return make_response(jsonify({'status': 0, "message": "Could not create a new user, Querying error!", "errorDB": err}), 500)
     finally:
         cursor.close()
-    return make_response(jsonify({'data':{'public_id':public_id}, 'status':1, 'message':'Sign up success'}), 201)
+    return make_response(jsonify({'data': {'public_id': public_id}, 'status': 1, 'message': 'Sign up success'}), 201)
 
 
 @app.route('/auth/signin', methods=['GET'])
@@ -84,10 +65,10 @@ def sign_in():
             return make_response(jsonify({'status': 0, 'message': 'Password is required!'}), 400)
     try:
         cursor = mysql.connection.cursor()
-        query = 'SELECT public_id, email, password, full_name, shop_name FROM user WHERE email = %s LIMIT 1;'
+        query = 'SELECT public_id, email, password, full_name, shop_name FROM User WHERE email = %s LIMIT 1;'
         params = [auth.username]
         user_data = cursor.execute(query, params)
-    except:
+    except ():
         return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
     if user_data > 0:
         detail_user = cursor.fetchall()
@@ -110,7 +91,7 @@ def public_id_required(f):
         try:
             cursor = mysql.connection.cursor()
             cursor.execute(
-                'SELECT user_id FROM user WHERE public_id = %s LIMIT 1;', [public_id])
+                'SELECT user_id FROM User WHERE public_id = %s LIMIT 1;', [public_id])
         except:
             return make_response(jsonify({'message': 'Public Id is Invalid!'}), 401)
         current_user = cursor.fetchall()
@@ -130,8 +111,9 @@ def create_goods(current_user):
     user_id = current_user[0][0]
     try:
         cursor = mysql.connection.cursor()
-        query = 'INSERT INTO goods(goods_name, goods_quantity, goods_unit, goods_price, user_id) VALUES(%s, %s, %s, %s, %s);'
-        params = (goods_name, float(goods_quantity), goods_unit, int(goods_price), int(user_id))
+        query = 'INSERT INTO Goods(goods_name, goods_quantity, goods_unit, goods_price, user_id) VALUES(%s, %s, %s, %s, %s);'
+        params = (goods_name, float(goods_quantity),
+                  goods_unit, int(goods_price), int(user_id))
         cursor.execute(query, params)
         mysql.connection.commit()
     except:
@@ -147,7 +129,7 @@ def get_all_goods(current_user):
     current_user = current_user[0][0]
     try:
         cursor = mysql.connection.cursor()
-        query = 'SELECT goods_id, goods_name, goods_quantity, goods_unit, goods_price FROM user JOIN goods ON user.user_id = goods.user_id WHERE user.user_id = %s;'
+        query = 'SELECT goods_id, goods_name, goods_quantity, goods_unit, goods_price FROM User JOIN Goods ON user.user_id = Goods.user_id WHERE user.user_id = %s;'
         params = [current_user]
         cursor.execute(query, params)
     except:
@@ -174,100 +156,63 @@ def get_all_goods(current_user):
     return make_response(jsonify(response), 200)
 
 
-@app.route('/warehouse/update/goods/increase', methods=['PUT'])
+@app.route('/warehouse/update/goods', methods=['PUT'])
 @public_id_required
-def goods_increase(current_user):
+def goods_out(current_user):
     data = request.json
     goods_id = data['goods_id']
     goods_name_update = data['goods_name']
     goods_qty_update = data['goods_quantity']
     goods_unit_update = data['goods_unit']
     goods_price_update = data['goods_price']
+    # Querying warehouse data
     current_user = current_user[0][0]
     try:
         cursor = mysql.connection.cursor()
-        query = "SELECT goods_quantity FROM goods WHERE goods.goods_id=%s;"
-        params = [goods_id]
+        query = "SELECT goods_id, goods_quantity FROM User join Goods ON User.user_id = Goods.user_id WHERE User.user_id=%s;"
+        params = [current_user]
         cursor.execute(query, params)
     except:
         return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
     data = cursor.fetchall()
     cursor.close()
-    quantity = data[0][0]
-    quantity += goods_qty_update
+    # finding specific goods
+    for x in data:
+        if x[0] == goods_id:
+            goods = x
+    # calculate differences between updated data and warehouse data, to make demand transaction for forecasting
+    if goods[1] > goods_qty_update:
+        demand = goods[1] - goods_qty_update
+        timeseries = datetime.datetime.now()
+        try:
+            cursor = mysql.connection.cursor()
+            query = "INSERT INTO WarehouseDemand(goods_id, qty, timeseries) VALUES(%s, %s, %s);"
+            params = (int(goods_id), float(demand), timeseries)
+            cursor.execute(query, params)
+            mysql.connection.commit()
+        except:
+            return make_response(jsonify({'status': 0, "message": "Could not create history transaction, Querying error!"}), 500)
+        cursor.close()
     try:
         cursor = mysql.connection.cursor()
-        query = "UPDATE goods SET goods_name = %s, goods_quantity = %s, goods_unit = %s, goods_price = %s WHERE goods_id = %s;"
-        params = (goods_name_update, float(quantity), goods_unit_update, int(goods_price_update), int(goods_id))
+        query = "UPDATE Goods SET goods_name = %s, goods_quantity = %s, goods_unit = %s, goods_price = %s WHERE goods_id = %s;"
+        params = (goods_name_update, float(goods_qty_update),
+                  goods_unit_update, int(goods_price_update), int(goods_id))
         cursor.execute(query, params)
         mysql.connection.commit()
     except:
-        return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
+        return make_response(jsonify({'status': 0, "message": "Could not update data warehouse, Querying error!"}), 500)
     cursor.close()
     response = {
-        "data": {
-            "goods_id": goods_id,
-            "goods_name": goods_name_update,
-            "goods_quantity": quantity,
-            "goods_unit": goods_unit_update,
-            "goods_price": goods_price_update
+        'updated_data': {
+            'goods_id': goods_id,
+            'goods_name': goods_name_update,
+            'goods_quantity': goods_qty_update,
+            'goods_unit': goods_unit_update,
+            'goods_price': goods_price_update
         },
-        "status": 1,
-        "message": "successfully increase goods stock"
-    }
-    return make_response(jsonify(response), 200)
-
-
-@app.route('/warehouse/update/goods/decrease', methods=['PUT'])
-@public_id_required
-def good_decrease(current_user):
-    data = request.json
-    goods_id = data['goods_id']
-    goods_name_update = data['goods_name']
-    goods_qty_update = data['goods_quantity']
-    goods_unit_update = data['goods_unit']
-    goods_price_update = data['goods_price']
-    current_user = current_user[0][0]
-    try:
-        cursor = mysql.connection.cursor()
-        query = "SELECT goods_quantity FROM goods WHERE goods.goods_id=%s;"
-        params = [goods_id]
-        cursor.execute(query, params)
-    except:
-        return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
-    data = cursor.fetchall()
-    cursor.close()
-    quantity = data[0][0]
-    quantity -= goods_qty_update
-    try:
-        cursor = mysql.connection.cursor()
-        query = "UPDATE goods SET goods_name = %s, goods_quantity = %s, goods_unit = %s, goods_price = %s WHERE goods_id = %s;"
-        params = (goods_name_update, float(quantity),goods_unit_update, int(goods_price_update), int(goods_id))
-        cursor.execute(query, params)
-        mysql.connection.commit()
-    except:
-        return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
-    cursor.close()
-    timeseries = datetime.datetime.now()
-    try:
-        cursor = mysql.connection.cursor()
-        query = "INSERT INTO warehousedemand(goods_id, qty, timeseries) VALUES(%s, %s, %s);"
-        params = (int(goods_id), float(goods_qty_update), timeseries)
-        cursor.execute(query, params)
-        mysql.connection.commit()
-    except:
-        return make_response(jsonify({'status': 0, "message": "Could not create history transaction, Querying error!"}), 500)
-    cursor.close()
-    response = {
-        "data": {
-            "goods_id": goods_id,
-            "goods_name": goods_name_update,
-            "goods_quantity": quantity,
-            "goods_unit": goods_unit_update,
-            "goods_price": goods_price_update
-        },
-        "status": 1,
-        "message": "successfully decrease goods stock"
+        'status': 1,
+        'message': 'Update data success'
     }
     return make_response(jsonify(response), 200)
 
@@ -277,7 +222,7 @@ def good_decrease(current_user):
 def delete_goods(current_user, goods_id):
     try:
         cursor = mysql.connection.cursor()
-        query = 'DELETE FROM goods WHERE goods_id = %s'
+        query = 'DELETE FROM Goods WHERE goods_id = %s'
         params = [goods_id]
         cursor.execute(query, params)
         mysql.connection.commit()
@@ -287,39 +232,34 @@ def delete_goods(current_user, goods_id):
     return make_response(jsonify({'status': 1, 'message': 'Delete data success'}), 200)
 
 
-@app.route('/warehouse/demand/predict/<goods_id>', methods=['POST'])
+@app.route('/warehouse/demand/predict/<goods_id>', methods=['GET'])
 @public_id_required
 def predict_demand(current_user, goods_id):
     # retrieves last 150 data from 'WarehouseDemand' table in a database
-    user_id = current_user[0][0]
     try:
         cursor = mysql.connection.cursor()
-        # data = cursor.execute('SELECT qty FROM Goods JOIN user ON Goods.user_id = User.user_id JOIN WarehouseDemand ON Goods.goods_id = WarehouseDemand.goods_id WHERE Goods.user_id=%s AND Goods.goods_id=%s;',(int(user_id), int(goods_id)))
-        query = 'SELECT qty  FROM warehousedemand WHERE goods_id=%s;'
+        query = 'SELECT qty  FROM WarehouseDemand WHERE goods_id=%s;'
         params = [int(goods_id)]
         data = cursor.execute(query, params)
     except:
         return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
-    if data < 150:
+    if data < 120:
         return make_response(jsonify({'status': 0, 'message': 'Not enough data to make predictions'}), 500)
     data = cursor.fetchall()
-    if len(data) > 150:
-        data = data[-150:]
+    if len(data) > 120:
+        data = data[-120:]
     tmp_data = []
     for x in data:
         tmp_data.append(x)
     df = pd.DataFrame.from_records(tmp_data, columns=['qty'])
     train = df['qty'].values.astype(np.float32).reshape(-1, 1)
-
     # ==== Preprocessing ====
     #Nomalize Data
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaler = scaler.fit(train)
     train = scaler.transform(train)
-
     # Make a DataFrame
     train = pd.DataFrame(train, columns=['qty'])
-
     # Creating Windowed Dataset
     def windowed_data(dataframe, size, start):
         container = []
@@ -337,7 +277,6 @@ def predict_demand(current_user, goods_id):
         return container
     x_train = windowed_data(train.values.tolist(), 13, 0)
     y_train = windowed_data(train.values.tolist(), 6, 14)
-
     # Convert into 2D
     def convert_2d(data):
         dim1 = []
@@ -350,79 +289,63 @@ def predict_demand(current_user, goods_id):
         return dim1
     x_train = convert_2d(x_train)
     y_train = convert_2d(y_train)
-
     # Selecting last value for predicting future value
     to_predict = np.array(x_train[-1], dtype=np.float32).reshape(1, -1)
     # Makes shape of x and y tobe equal
     x_train = x_train[:len(y_train)]
-
     # ====== Modeling ======
-    # Tuning Hyperparameters
-    # hyperparameters = {
-    #     'n_estimators': [200, 400, 600],
-    #     'min_samples_leaf': [1,2,3,4,5],
-    #     'min_samples_split': [2,4,6,8]
-    # }
-
-    # model = make_pipeline(
-    #     GridSearchCV(
-    #         RandomForestRegressor(),
-    #         param_grid=hyperparameters,
-    #         cv=3,
-    #         refit=True
-    #     )
-    # )
-    # model.fit(x_train, y_train)
-    # result = model.predict(to_predict)
-    # print(result)
-
-    model = RandomForestRegressor(n_estimators=200, min_samples_leaf=2, min_samples_split=10, bootstrap=False, criterion='mae')
+    model = RandomForestRegressor(
+        n_estimators=200, min_samples_leaf=2, min_samples_split=10, bootstrap=False, criterion='mae')
     model.fit(x_train, y_train)
     prediction = model.predict(to_predict)
     prediction = scaler.inverse_transform(prediction)
     timenow = datetime.datetime.now()
-    try:
-        cursor = mysql.connection.cursor()
-        query = 'INSERT INTO demandprediction(goods_id, user_id, day_1, day_2, day_3, day_4, day_5, day_6, day_7, start_date_prediction) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
-        params = (int(goods_id), int(user_id), float(prediction[0][0]), float(prediction[0][1]), float(prediction[0][2]), float(
-            prediction[0][3]), float(prediction[0][4]), float(prediction[0][5]), float(prediction[0][6]), timenow)
-        cursor.execute(query, params)
-        mysql.connection.commit()
-    except:
-        return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
-    cursor.close()
-    return make_response(jsonify({'status': 1, 'message': 'Prediction done'}), 201)
-
-
-@app.route('/warehouse/demand/get/prediction', methods=['GET'])
-@public_id_required
-def get_demand_prediction(current_user):
-    user_id = current_user[0][0]
-    try:
-        cursor = mysql.connection.cursor()
-        query = 'SELECT goods_name, day_1, day_2, day_3, day_4, day_5, day_6, day_7, start_date_prediction FROM demandprediction JOIN goods ON demandprediction.goods_id = goods.goods_id WHERE demandprediction.user_id = %s;'
-        params = [user_id]
-        data = cursor.execute(query, params)
-    except:
-        return make_response(jsonify({'status': 0, 'message': "Querying error!"}), 500)
-    if not data > 0:
-        return make_response(jsonify({'status': 0, 'message': "Empty Data!"}), 200)
-    data = cursor.fetchall()
-    detail_data = []
-    for x in data:
-        n_data = []
-        for inner in x:
-            n_data.append(inner)
-        detail_data.append(n_data)
+    timenow = timenow.timetuple()
+    year = timenow[0]
+    month = timenow[1]
+    date = timenow[2]
+    date_list = []
+    for x in range(0, 7):
+        dmy = ""
+        date += 1
+        dmy += f"{date}/{month}/{year}"
+        date_list.append(dmy)
+    print(date_list)
     response = {
-        'data': {
-            'detail_data': detail_data,
-            'total_data': len(detail_data),
-            'inner_data': len(detail_data[0])
+        "data": {
+            "day1": {
+                "date": date_list[0],
+                "prediction": int(round(prediction[0][0]))
+            },
+            "day2": {
+                "date": date_list[1],
+                "prediction": int(round(prediction[0][1]))
+            },
+            "day3": {
+                "date": date_list[2],
+                "prediction": int(round(prediction[0][2]))
+            },
+            "day4": {
+                "date": date_list[3],
+                "prediction": int(round(prediction[0][3]))
+            },
+            "day5": {
+                "date": date_list[4],
+                "prediction": int(round(prediction[0][4]))
+            },
+            "day6": {
+                "date": date_list[5],
+                "prediction": int(round(prediction[0][5]))
+            },
+            "day7": {
+                "date": date_list[6],
+                "prediction": int(round(prediction[0][6]))
+            }
         },
-        'status': 1,
-        'message': 'Success get data prediction'
+        "message": "Success Predicting Data",
+        "status": 1
     }
+    cursor.close()
     return make_response(jsonify(response), 200)
 
 
